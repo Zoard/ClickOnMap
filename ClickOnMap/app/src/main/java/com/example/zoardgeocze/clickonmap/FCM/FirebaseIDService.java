bbbac0ae9fb086fa5b4eb02ce852e4334be4d728
Zoárd Geöcze <zoardag@gmail.com>
2017-05-03 17:49:54 -0300

    Firebase Added

--- ClickOnMap/app/src/main/java/com/example/zoardgeocze/clickonmap/FCM/FirebaseIDService.java
+++ ClickOnMap/app/src/main/java/com/example/zoardgeocze/clickonmap/FCM/FirebaseIDService.java
@@ -6,7 +6,7 @@ import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.FirebaseInstanceIdService;
 
 /**
- * Created by zoard_000 on 03/05/2017.
+ * Created by ZoardGeocze on 03/05/2017.
  */
 
 public class FirebaseIDService extends FirebaseInstanceIdService {
@@ -16,7 +16,7 @@ public class FirebaseIDService extends FirebaseInstanceIdService {
     public void onTokenRefresh() {
         // Get updated InstanceID token.
         String refreshedToken = FirebaseInstanceId.getInstance().getToken();
-        Log.d(TAG, "Refreshed token: " + refreshedToken);
+        Log.i(TAG, "Refreshed token: " + refreshedToken);
 
         // TODO: Implement this method to send any registration to your app's servers.
         sendRegistrationToServer(refreshedToken);
