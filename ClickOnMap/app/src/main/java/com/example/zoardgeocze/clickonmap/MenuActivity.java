f2be8f020066bb1985fbcfb3495fe1747061b028
Zoárd Geöcze <zoardag@gmail.com>
2017-06-23 17:36:35 -0300

    Implementing New Click On Map Data Base Schema (Not Finished)

--- ClickOnMap/app/src/main/java/com/example/zoardgeocze/clickonmap/MenuActivity.java
+++ ClickOnMap/app/src/main/java/com/example/zoardgeocze/clickonmap/MenuActivity.java
@@ -62,38 +62,10 @@ public class MenuActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onRestart() {
-        super.onRestart();
-        Intent data = getIntent();
-
-        if(data != null) {
-            Log.d("cadastro", "tem intent");
-            Bundle bundle = data.getExtras();
-
-            if(bundle != null) {
-                Log.d("cadastro", "tem bundle");
-                VGISystem vgiSystem = (VGISystem) bundle.getSerializable("vgiSystem");
-                User user = (User) bundle.getSerializable("user");
-
-                Log.d("cadastro", vgiSystem.getName() + " - " + user.getName());
-
-                this.generalController.registerUserSystem(this, vgiSystem, user);
-                this.generalController.registerDeviceSystem(this, vgiSystem);
-
-                SystemTile systemTile = new SystemTile(vgiSystem.getName(), vgiSystem);
-
-                this.menuTiles.add(0, systemTile);
-            }
-        }
-
-    }
-
-    @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if(resultCode == 1) {
-            Log.d("gambiarra", "cheguei onActivityResult()");
-            /*Bundle bundle = data.getExtras();
+            Bundle bundle = data.getExtras();
             VGISystem vgiSystem = (VGISystem) bundle.getSerializable("vgiSystem");
             User user = (User) bundle.getSerializable("user");
 
@@ -104,7 +76,7 @@ public class MenuActivity extends AppCompatActivity {
 
             this.menuTiles.add(0,systemTile);
 
-            //Toast.makeText(this,String.valueOf(vgiSystem.getContributions()),Toast.LENGTH_SHORT).show();*/
+            //Toast.makeText(this,String.valueOf(vgiSystem.getContributions()),Toast.LENGTH_SHORT).show();
 
         }
     }
