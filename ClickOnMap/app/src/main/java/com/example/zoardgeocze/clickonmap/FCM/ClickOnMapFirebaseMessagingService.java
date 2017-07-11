553aeac73474d31f63e0a56ddf7babc494e98209
Zoárd Geöcze <zoardag@gmail.com>
2017-07-11 17:04:20 -0300

    Starting communication with Main Server using FCM

--- ClickOnMap/app/src/main/java/com/example/zoardgeocze/clickonmap/FCM/ClickOnMapFirebaseMessagingService.java
+++ ClickOnMap/app/src/main/java/com/example/zoardgeocze/clickonmap/FCM/ClickOnMapFirebaseMessagingService.java
@@ -1,9 +1,14 @@
 package com.example.zoardgeocze.clickonmap.FCM;
 
+import android.content.Intent;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Map;
 
 /**
  * Created by ZoardGeocze on 03/05/2017.
@@ -18,6 +23,30 @@ public class ClickOnMapFirebaseMessagingService extends FirebaseMessagingService
         // Also if you intend on generating your own notifications as a result of a received FCM
         // message, here is where that should be initiated.
         Log.i(TAG, "From: " + remoteMessage.getFrom());
+
+        if (remoteMessage.getData().size() > 0) {
+            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+
+            Map<String, String> data = remoteMessage.getData();
+            if(data.size() > 0){
+                //String msg = data.get("msg");
+                String msg = "";
+                // Decodificando a msg
+                try {
+                    msg = new String(data.get("msg").getBytes(), "UTF-8");
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
+
+                Log.i(TAG, "Msg: " + msg);
+
+                String horario = data.get("horario");
+
+                Log.i(TAG, "Horario: " + horario);
+
+            }
+        }
+
         Log.i(TAG, "Notification Message Body: " + remoteMessage.getNotification().getBody());
     }
 }
